fad808a94412b5758af4e9f9c544af544dee9e6e
Kguswo <Kguswo@users.noreply.github.com>
2025-09-08 01:43:20 +0000

    deploy: d2c572b164ccce9a71845ad8b7170a06121598ea

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757209376',resources: [
+    cacheName: 'chirpy-1757295789',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
